6a4bb5e649640e6d6f3ccc4db29ccf88423463d0
QuasiStellar18 <89459546+QuasiStellar18@users.noreply.github.com>
2026-06-13 13:49:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,9 +5,7 @@ const ASSETS = [
     "./style.css?v=5",
     "./db.js?v=5",
     "./app.js?v=5",
-    "./manifest.json",
-    "./icons/icon-192.png",
-    "./icons/icon-512.png"
+    "./manifest.json"
 ];
 
 self.addEventListener("install", (event) => {
